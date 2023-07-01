67bf5f80c8d368747b31e57cd6089c9edfeff133
Semhar Tesfu <ccaeste@ucl.ac.uk>
2023-07-01 20:52:15 +0100

    new react app

---
+++ materni-learn/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ForYouPage } from './pages';
+
+function App() {
+  return (
+    <div className="App">
+    <ForYouPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ materni-learn/src/pages/ForYouPage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function ForYouPage(){
+    return (
+        <div className="container-fluid">
+            <h1>Updates</h1>
+        </div>
+    )
+}
